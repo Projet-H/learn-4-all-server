72dde441e42da28e79e8acb894db0e87472712bc
DAOUD-07362 <gabriel.daoud@assurance-maladie.fr>
2020-04-03 20:38:18 +0200

    Move host client to env

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://learn4all.fr",
+    origin: process.env.CLIENT_HOST,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
