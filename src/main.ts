d4690f3769199bd5fc4af7d7a78cd0d8288586fe
DAOUD-07362 <gabriel.daoud@assurance-maladie.fr>
2020-04-03 20:34:44 +0200

    Add Cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "https://learn4all.fr",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
